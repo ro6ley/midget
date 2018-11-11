de2fd837fa2ab801945dc489140cc54160093d2a
Robley <robleyadrian@gmail.com>
2018-11-11 22:48:26 +0300

    add documentation path.

--- midget/urls.py
+++ midget/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
+    path('docs/', include_docs_urls(title='Midget URLShortener API'))
 ]
