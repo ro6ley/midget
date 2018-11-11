e7a8f92475857ccea44ba65138bf35f4cf7ef31b
Robley <robleyadrian@gmail.com>
2018-11-11 21:53:54 +0300

    refactor shortener functionality.

--- midget/api/shortener.py
+++ midget/api/shortener.py
@@ -26,29 +26,19 @@ def generate_random_string(string_length=7):
     return random[0:string_length]
 
 
-def check_random_string(random_string):
+def create_url(url_to_be_shortened):
     """
     Check if a string is already in use on the redis server and create a new
     one if needed.
 
-    If it exists, return true, else return false
-    """
-    if redis_instance.get(random_string):
-        return True
-    else:
-        return False
-
-
-def create_url(url_to_be_shortened):
-    """
     Create the shortened URL and save it on Redis
     """
     strng = generate_random_string()
 
-    if check_random_string(strng):
+    if redis_instance.get(strng):
         create_url(url_to_be_shortened)
 
-    elif not check_random_string(strng):
+    elif not redis_instance.get(strng):
         redis_instance.set(strng, url_to_be_shortened)
         return strng
 
